641bba40ee72d2951262fafce897c9ef2f13dbcf
liutiejun <liutiejun@unioncast.cn>
2017-05-21 19:44:04 +0800

    增加response

--- ci-core/src/main/java/org/yansou/ci/core/rest/response/project/BiddingDataArrayResponse.java
+++ ci-core/src/main/java/org/yansou/ci/core/rest/response/project/BiddingDataArrayResponse.java
@@ -6,4 +6,5 @@ import org.yansou.ci.core.rest.response.RestResponse;
 public class BiddingDataArrayResponse extends RestResponse<BiddingData[]> {
 
 	private static final long serialVersionUID = 1661358667356220830L;
+
 }
---
+++ ci-core/src/main/java/org/yansou/ci/core/rest/response/project/PlanBuildDataResponse.java
@@ -0,0 +1,10 @@
+package org.yansou.ci.core.rest.response.project;
+
+import org.yansou.ci.core.model.project.PlanBuildData;
+import org.yansou.ci.core.rest.response.RestResponse;
+
+public class PlanBuildDataResponse extends RestResponse<PlanBuildData> {
+
+	private static final long serialVersionUID = -6622172325896891936L;
+
+}
