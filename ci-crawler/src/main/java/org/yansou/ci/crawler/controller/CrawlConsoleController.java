aef5e9f1979b86c73500b1acc1c68f5ca252d08f
WIN-DELL\Administrator <273105667@qq.com>
2017-06-09 19:08:14 +0800

    Signed-off-by: WIN-DELL\Administrator <273105667@qq.com>

--- ci-crawler/src/main/java/org/yansou/ci/crawler/controller/CrawlConsoleController.java
+++ ci-crawler/src/main/java/org/yansou/ci/crawler/controller/CrawlConsoleController.java
@@ -1,17 +1,17 @@
 package org.yansou.ci.crawler.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yansou.ci.core.crawl.model.CrawlStatus;
+import org.yansou.ci.crawler.core.ConsoleManager;
 
 @Controller
 @RequestMapping("/console")
 public class CrawlConsoleController {
-	
-	
-	
-	
+	@Autowired
+	private ConsoleManager consoleManager;
 	@GetMapping("/status")
 	public CrawlStatus status() {
 		return null;
