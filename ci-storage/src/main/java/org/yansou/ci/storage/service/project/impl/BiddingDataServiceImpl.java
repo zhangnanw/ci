1d76a7a5a565882849af40905b4cdff75fbba627
liutiejun <liutiejun@unioncast.cn>
2017-06-10 19:00:58 +0800

    保存数据的时候增加状态字段的更新

--- ci-storage/src/main/java/org/yansou/ci/storage/service/project/impl/BiddingDataServiceImpl.java
+++ ci-storage/src/main/java/org/yansou/ci/storage/service/project/impl/BiddingDataServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.yansou.ci.common.exception.DaoException;
+import org.yansou.ci.core.db.model.AbstractModel;
 import org.yansou.ci.core.db.model.project.BiddingData;
 import org.yansou.ci.core.db.model.project.SnapshotInfo;
 import org.yansou.ci.storage.common.repository.GeneralRepository;
@@ -72,6 +73,8 @@ public class BiddingDataServiceImpl extends GeneralServiceImpl<BiddingData, Long
 
 			entity.setProductType(productType);
 		}
+
+		entity.setStatus(AbstractModel.Status.NORMAL.getValue());
 
 		return biddingDataRepository.save(entity);
 	}
