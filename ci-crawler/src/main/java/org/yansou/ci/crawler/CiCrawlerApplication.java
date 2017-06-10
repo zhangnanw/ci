e9160e88e40ca7f2bafdca187fbc4de87cb13689
273105667@qq.com <273105667@qq.com>
2017-06-10 12:34:10 +0800

    Signed-off-by: 273105667@qq.com <273105667@qq.com>

--- ci-crawler/src/main/java/org/yansou/ci/crawler/CiCrawlerApplication.java
+++ ci-crawler/src/main/java/org/yansou/ci/crawler/CiCrawlerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.yansou.ci.crawler.core.HngpHttpClientDownloader;
 
 import us.codecraft.webmagic.downloader.HttpClientDownloader;
 
