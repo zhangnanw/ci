b8e17ea6f9df2c56f900ca151d8e9c337e7151a6
WIN-DELL\Administrator <273105667@qq.com>
2017-06-12 15:51:38 +0800

    增加两个项目合并用的类。

---
+++ ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectVector.java
@@ -0,0 +1,17 @@
+package org.yansou.ci.storage.merge;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class ProjectVector {
+    //第一级地址
+    private String a1;
+    //第二级地址
+    private String a2;
+    //第三级地址
+    private String a3;
+    //功率
+    private String mw1;
+    //甲方
+    private String party_a;
+}
