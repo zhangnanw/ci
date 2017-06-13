5ab962c14360c4176e0cfc118738cc9fdc913902
WIN-DELL\Administrator <273105667@qq.com>
2017-06-13 11:06:04 +0800

    Signed-off-by: WIN-DELL\Administrator <273105667@qq.com>

--- ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectMergeProcess.java
+++ ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectMergeProcess.java
@@ -1,7 +1,6 @@
 package org.yansou.ci.storage.merge;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.yansou.ci.storage.ciimp.Dlzb2PlanBuildDataInfo;
 import org.yansou.ci.storage.service.project.BiddingDataService;
 import org.yansou.ci.storage.service.project.PlanBuildDataService;
 
@@ -9,14 +8,13 @@ import org.yansou.ci.storage.service.project.PlanBuildDataService;
  * Created by Administrator on 2017/6/12.
  */
 public class ProjectMergeProcess implements Runnable {
-    @Autowired
-    PlanBuildDataService planBuildDataService;
-    @Autowired
-    BiddingDataService biddingDataService;
+	@Autowired
+	PlanBuildDataService planBuildDataService;
+	@Autowired
+	BiddingDataService biddingDataService;
 
+	@Override
+	public void run() {
 
-    @Override
-    public void run() {
-        
-    }
+	}
 }
