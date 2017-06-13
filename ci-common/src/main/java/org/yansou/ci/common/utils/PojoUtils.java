548cfa6dd793607d9d73e18979e426fceb359ffa
WIN-DELL\Administrator <273105667@qq.com>
2017-06-13 16:55:08 +0800

    Signed-off-by: WIN-DELL\Administrator <273105667@qq.com>

--- ci-common/src/main/java/org/yansou/ci/common/utils/PojoUtils.java
+++ ci-common/src/main/java/org/yansou/ci/common/utils/PojoUtils.java
@@ -4,6 +4,9 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -31,6 +34,55 @@ public final class PojoUtils {
 				String str = (String) field.get(obj);
 				if (null != str) {
 					field.set(obj, str.trim());
+				}
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				throw new IllegalStateException(e);
+			}
+		}
+	}
+
+	/**
+	 * 所有 为null的字符串变成空白字符串
+	 * 
+	 * @param obj
+	 */
+	final static public void nullStringToEmpty(Object obj) {
+		_allFieldFun(obj, String.class, Objects::isNull, val -> "");
+	}
+
+	/**
+	 * 所有字段处理。
+	 * 
+	 * @param obj
+	 * @param fieldType
+	 * @param valPred
+	 * @param fun
+	 */
+	final static void _allFieldFun(Object obj, Class<?> fieldType, Predicate<Object> valPred,
+			Function<Object, Object> fun) {
+		_allFieldFun(obj, f -> String.class.equals(f.getType()), valPred, fun);
+
+	}
+
+	/**
+	 * 所有字段处理。
+	 * 
+	 * @param obj
+	 * @param classPred
+	 * @param valPred
+	 * @param fun
+	 */
+	final static void _allFieldFun(Object obj, Predicate<Field> classPred, Predicate<Object> valPred,
+			Function<Object, Object> fun) {
+		Class<?> clazz = obj.getClass();
+		List<Field> fieldList = Arrays.asList(clazz.getDeclaredFields()).stream()
+				.filter(f -> !Modifier.isFinal(f.getModifiers())).filter(f -> !Modifier.isStatic(f.getModifiers()))
+				.filter(classPred).peek(m -> m.setAccessible(true)).collect(Collectors.toList());
+		for (Field field : fieldList) {
+			try {
+				Object val = field.get(obj);
+				if (valPred.test(val)) {
+					field.set(obj, fun.apply(obj));
 				}
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				throw new IllegalStateException(e);
--- ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectMergeProcess.java
+++ ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectMergeProcess.java
@@ -1,7 +1,7 @@
 package org.yansou.ci.storage.merge;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.yansou.ci.storage.ciimp.Dlzb2PlanBuildDataInfo;
+import org.yansou.ci.common.exception.DaoException;
 import org.yansou.ci.storage.service.project.BiddingDataService;
 import org.yansou.ci.storage.service.project.PlanBuildDataService;
 
@@ -9,14 +9,17 @@ import org.yansou.ci.storage.service.project.PlanBuildDataService;
  * Created by Administrator on 2017/6/12.
  */
 public class ProjectMergeProcess implements Runnable {
-    @Autowired
-    PlanBuildDataService planBuildDataService;
-    @Autowired
-    BiddingDataService biddingDataService;
+	@Autowired
+	PlanBuildDataService planBuildDataService;
+	@Autowired
+	BiddingDataService biddingDataService;
 
-
-    @Override
-    public void run() {
-
-    }
+	@Override
+	public void run() {
+		try {
+			planBuildDataService.findAll();
+		} catch (DaoException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
