f7d36c277ccaed969a42fe4f23cafcb05a044c85
xiaoceng <xiaoceng@192.168.99.1>
2017-06-15 21:33:52 +0800

    Signed-off-by: xiaoceng <xiaoceng@192.168.99.1>

--- ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectMergeProcess.java
+++ ci-storage/src/main/java/org/yansou/ci/storage/merge/ProjectMergeProcess.java
@@ -1,7 +1,6 @@
 package org.yansou.ci.storage.merge;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.yansou.ci.storage.ciimp.Dlzb2PlanBuildDataInfo;
 import org.yansou.ci.storage.service.project.BiddingDataService;
 import org.yansou.ci.storage.service.project.PlanBuildDataService;
 
