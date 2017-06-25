aa168049cab67f0372466c2802cf010646136cf2
273105667@qq.com <273105667@qq.com>
2017-06-25 21:54:21 +0800

    Merge remote-tracking branch 'origin/liutiejun' into 2017-06-01-n.zhang
    Conflicts:
    	ci-core/src/main/java/org/yansou/ci/core/db/model/project/PlanBuildData.java
    	ci-core/src/main/java/org/yansou/ci/core/db/model/project/PriceTrackingInfo.java

--- ci-storage/src/main/java/org/yansou/ci/storage/ciimp/CorvToRecordData.java
+++ ci-storage/src/main/java/org/yansou/ci/storage/ciimp/CorvToRecordData.java
@@ -2,6 +2,7 @@ package org.yansou.ci.storage.ciimp;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,8 @@ import org.yansou.ci.core.db.model.project.SnapshotInfo;
 import org.yansou.ci.data.mining.analyzer.impl.AreaAnalyzer;
 import org.yansou.ci.storage.service.project.RecordDataService;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @Component
@@ -21,19 +24,21 @@ public class CorvToRecordData extends AbsStatistics {
 
     public void run() {
         try {
-            JSONArray arr = qr.query("SELECT * FROM tab_put_on_record WHERE url not in(SELECT url FROM `" + TmpConfigRead.getCfgName() + "`.ci_record_data)", JSONArrayHandler.create());
+            JSONArray arr = qr.query( "SELECT * FROM tab_put_on_record WHERE url not in(SELECT url FROM `" + TmpConfigRead.getCfgName() + "`.ci_record_data)", JSONArrayHandler.create() );
             for (int i = 0; i < arr.size(); i++) {
-                JSONObject obj = arr.getJSONObject(i);
-                RecordData rd = new PutIn2RecordData(obj).get();
+                JSONObject obj = arr.getJSONObject( i );
+                RecordData rd = new PutIn2RecordData( obj ).get();
                 SnapshotInfo snap = new SnapshotInfo();
                 String snapshotId = UUID.randomUUID().toString();
-                snap.setSnapshotId(snapshotId);
-                snap.setDataType(3);
-                rd.setSnapshotId(snapshotId);
-                recordDataService.saveDataAndSnapshotInfo(rd, snap);
+                snap.setSnapshotId( snapshotId );
+                snap.setDataType( 3 );
+
+                snap.setContext( obj.getString( "context" ) );
+                rd.setSnapshotId( snapshotId );
+                recordDataService.saveDataAndSnapshotInfo( rd, snap );
             }
         } catch (Exception e) {
-            throw new IllegalStateException(e);
+            throw new IllegalStateException( e );
         }
     }
 
@@ -46,14 +51,21 @@ public class CorvToRecordData extends AbsStatistics {
 
         public RecordData get() {
             RecordData rd = new RecordData();
-            rd.setUrl(srcObj.getString("url"));
-            rd.setProjectName(srcObj.getString("title"));
-            if (StringUtils.isBlank(rd.getProjectName())) {
-                rd.setProjectName(srcObj.getString("anchor_text"));
+            rd.setUrl( srcObj.getString( "url" ) );
+            rd.setHtmlSource( srcObj.getString( "context" ) );
+            rd.setProjectName( srcObj.getString( "title" ) );
+            if (StringUtils.isBlank( rd.getProjectName() )) {
+                rd.setProjectName( srcObj.getString( "anchor_text" ) );
             }
-            rd.setProjectProvince(toProvince());
-            rd.setProjectCity(toCity());
-            rd.setProjectDistrict(toDistrict());
+            String projectProvince = toProvince();
+            if (org.apache.commons.lang3.StringUtils.isNotBlank( projectProvince )) {
+                codeMap.keySet().stream().filter( Objects::nonNull ).filter( key -> key.contains( projectProvince ) )
+                        .forEach( key -> {
+                            rd.setProjectProvince( codeMap.get( key ) );
+                        } );
+            }
+            rd.setProjectCity( toCity() );
+            rd.setProjectDistrict( toDistrict() );
             return rd;
         }
 
@@ -61,32 +73,42 @@ public class CorvToRecordData extends AbsStatistics {
          * 地区解析器
          */
         private static AreaAnalyzer AREA_ANALYZER = AreaAnalyzer.getExamples();
+        final static private Map<String, Integer> codeMap = Maps.newHashMap();
+
+        static {
+            String[] arr = "1:北京市;2:天津市;3:上海市;4:重庆市;5:安徽省;6:福建省;7:甘肃省;8:广东省;9:贵州省;10:海南省;11:河北省;12:河南省;13:湖北省;14:湖南省;15:吉林省;16:江苏省;17:江西省;18:辽宁省;19:青海省;20:山东省;21:山西省;22:陕西省;23:四川省;24:云南省;25:浙江省;26:台湾省;27:黑龙江省;28:西藏自治区;29:内蒙古自治区;30:宁夏回族自治区;31:广西壮族自治区;32:新疆维吾尔自治区;33:香港特别行政区;34:澳门特别行政区"
+                    .split( ";" );
+            for (String line : arr) {
+                String[] as = line.split( ":" );
+                codeMap.put( as[1], Integer.valueOf( as[0] ) );
+            }
+        }
 
         private String toProvince() {
-            JSONObject res = AREA_ANALYZER.analy(srcObj);
-            JSONArray arr = res.getJSONArray("area");
-            if (JSONUtils.isEmpty(arr)) {
+            JSONObject res = AREA_ANALYZER.analy( srcObj );
+            JSONArray arr = res.getJSONArray( "area" );
+            if (JSONUtils.isEmpty( arr )) {
                 return null;
             }
-            return arr.getString(0);
+            return arr.getString( 0 );
         }
 
         private String toCity() {
-            JSONObject res = AREA_ANALYZER.analy(srcObj);
-            JSONArray arr = res.getJSONArray("area");
-            if (JSONUtils.isEmpty(arr) || arr.size() <= 1) {
+            JSONObject res = AREA_ANALYZER.analy( srcObj );
+            JSONArray arr = res.getJSONArray( "area" );
+            if (JSONUtils.isEmpty( arr ) || arr.size() <= 1) {
                 return null;
             }
-            return arr.getString(1);
+            return arr.getString( 1 );
         }
 
         private String toDistrict() {
-            JSONObject res = AREA_ANALYZER.analy(srcObj);
-            JSONArray arr = res.getJSONArray("area");
-            if (JSONUtils.isEmpty(arr) || arr.size() <= 2) {
+            JSONObject res = AREA_ANALYZER.analy( srcObj );
+            JSONArray arr = res.getJSONArray( "area" );
+            if (JSONUtils.isEmpty( arr ) || arr.size() <= 2) {
                 return null;
             }
-            return arr.getString(2);
+            return arr.getString( 2 );
         }
     }
 
